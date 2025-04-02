5c27e4b0ce7b3796aeceb6dce8dda156795cc0cf
shreeramansrinivasan <shreeramansrinivasan@users.noreply.github.com>
2025-04-02 07:10:17 -0700

    initial commits for reed solomon codes

---
+++ Reed_Solomon_codes/encoding.py
@@ -0,0 +1,5 @@
+def reed_solomon_encoding():
+    """Trying to figure out how to write the encoder"""
+    """Planning to implement the systematic version of the reed solomon encoding algorithm"""
+    """Systematic version means that the encoding input words are part of the final message word"""
+    
