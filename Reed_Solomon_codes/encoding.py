ea6cf5cc4ae8ba62c23d79ccaacf02ff05fe5e58
shreeramansrinivasan <shreeramansrinivasan@users.noreply.github.com>
2025-04-10 07:51:54 -0700

    reed solomon encoder

--- Reed_Solomon_codes/encoding.py
+++ Reed_Solomon_codes/encoding.py
@@ -1,4 +1,16 @@
 import numpy as np
+import galois
+def primitive_elements(m):
+    GF = galois.GF(2**m)
+    return GF.primitive_element
+def generator_poly(k,n,m):
+    """Computes the generator polynomial of reed solomon code"""
+    GF = galois.GF(2**m)
+    alpha = GF.primitive_element
+    roots = []
+    for i in range(n-k,n):
+        roots.append(alpha**i)
+    return galois.Poly(roots,GF)
 def reed_solomon_encoding(m,n,k,input_words):
     """Trying to figure out how to write the encoder"""
     """Planning to implement the systematic version of the reed solomon encoding algorithm"""
@@ -8,4 +20,16 @@ def reed_solomon_encoding(m,n,k,input_words):
     input words is of length k message symbols"""
     if len(input_words) != k:
         raise ValueError("Input words length must be equal to k")
-    
+    #To construct the final output, take the input polynomial , 
+    # scale it by x^(n-k)
+    # find the remainder of the division by th generator polynomial 
+    # construct the message by concatenating the input words and the remainder 
+    # return the message 
+    GF = galois.GF(2**m)
+    message_word = galois.Poly([input_words[i] for i in range(k)],GF)
+    append_zeros = galois.Poly([0 for i in range(n-k)],GF)
+    message_word = message_word + append_zeros
+    gen_poly = generator_poly(k,n,m)
+    remainder = message_word % gen_poly
+    message_word = message_word + remainder
+    return message_word
